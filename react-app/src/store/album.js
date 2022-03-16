c23f35f3f4970c694098ac92ec477c45cc6f2cac
Srdan <srdan.vorkapic@outlook.com>
2022-03-16 11:25:03 -0700

    Action creators for album reducer

--- react-app/src/store/album.js
+++ react-app/src/store/album.js
@@ -4,4 +4,24 @@ const POST_ALBUM = "album/postAlbum";
 const PATCH_ALBUM = "album/patchAlbum";
 const DELETE_ALBUM = "album/deleteAlbum";
 
+// Action Creators
+
+export const getCurrentUserAlbumsActionCreator = albums => {
+    return { type: GET_CURRENT_USER_ALBUMS, albums };
+};
+
+export const postAlbumActionCreator = album => {
+    return { type: POST_ALBUM, album };
+};
+
+export const patchAlbumActionCreator = album => {
+    return { type: PATCH_ALBUM, album };
+};
+
+export const deleteAlbumActionCreator = id => {
+    return { type: DELETE_ALBUM, id };
+};
+
+// Thunk Creator for GET request
+
 
