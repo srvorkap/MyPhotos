d2843899e9eca90f975e2134c33d613faa5b3413
Srdan <srdan.vorkapic@outlook.com>
2022-03-16 11:24:16 -0700

    Action types for album reducer

---
+++ react-app/src/store/album.js
@@ -0,0 +1,7 @@
+// Action Types
+const GET_CURRENT_USER_ALBUMS = "album/getCurrentUserAlbums";
+const POST_ALBUM = "album/postAlbum";
+const PATCH_ALBUM = "album/patchAlbum";
+const DELETE_ALBUM = "album/deleteAlbum";
+
+
