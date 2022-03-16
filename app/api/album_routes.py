1fb0aea6829e3ca8efecff95c47b750bccd0069b
Srdan <srdan.vorkapic@outlook.com>
2022-03-15 20:22:35 -0700

    Patch album route

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -1,11 +1,14 @@
+from crypt import methods
 from flask import Blueprint, jsonify, session, request
 from app.models import db, User, Album, Photo
 from flask_login import current_user
+from app.forms.album_form import AlbumForm
+from app.api.auth_routes import validation_errors_to_error_messages
 
 album_routes = Blueprint('album', __name__)
 
 
-@album_routes.get('/')
+@album_routes.route('/')
 def get_all_albums():
     albums = Album.query.all()
     print(albums)
@@ -15,3 +18,48 @@ def get_all_albums():
     # album = Album.query.get(1)
     # album_dict = album.to_dict()
     # return { 'album': album_dict}
+
+
+@album_routes.route('/', methods=['POST'])
+def post_album():
+    current_user_id = 1 #current_user.get_id()
+    form = AlbumForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    data = form.data
+    print(form.data)
+
+    if form.validate_on_submit():
+        title = data['title']
+        description = data['description']
+
+        album = Album(
+            title = title,
+            description = description,
+            user_id = current_user_id
+        )
+
+        db.session.add(album)
+        db.session.commit()
+        return { 'album': album.to_dict()}
+    return { 'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+@album_routes.route('/<int:album_id>', methods=['PATCH'])
+def patch_album(album_id):
+    form = AlbumForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        album = Album.query.get(album_id)
+
+        data = form.data
+        album.title = data['title']
+        album.description = data['description']
+
+        db.session.commit()
+        return { 'album': album.to_dict()}
+    return { 'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+# @album_routes.route('/<int:album_id>')
+# def delete_album(album_id):
+#     photos =
--- app/forms/album_form.py
+++ app/forms/album_form.py
@@ -9,5 +9,4 @@ class AlbumForm(FlaskForm):
     Length(max=30, message="Title must be < 30 characters."),
     Length(min=2, message="Title must be > 1 character.")])
 
-    description = StringField('description', validators=[Length(max=255, message="Description must be < 255 characters"),
-    Length(min=2, message="Description must be > 1 character")])
+    description = StringField('description', validators=[Length(max=255, message="Description must be < 255 characters")])
