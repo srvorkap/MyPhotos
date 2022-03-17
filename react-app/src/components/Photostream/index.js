577488a75a6b8c73e1d1ee59b575966e2d888b83
Srdan <srdan.vorkapic@outlook.com>
2022-03-17 15:11:52 -0700

    Working on explore

--- react-app/src/components/Photostream/index.js
+++ react-app/src/components/Photostream/index.js
@@ -1,11 +1,11 @@
-import { Redirect } from "react-router-dom"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { getPhotos } from "../../store/photo"
+import { NavLink, Redirect } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPhotos } from "../../store/photo";
 
 const Photostream = ({ sessionUser }) => {
     const allPhotosArr = useSelector(store => store?.photo?.photos);
-    console.log('----------------', allPhotosArr)
+    console.log("----------------", allPhotosArr);
     let sessionUserPhotos;
     if (allPhotosArr) {
         // const allPhotosArr = Object?.values(allPhotosObj);
@@ -14,24 +14,27 @@ const Photostream = ({ sessionUser }) => {
             photo => photo?.user_id === sessionUser?.id
         );
     }
-    console.log('000000000000000', sessionUserPhotos)
+    console.log("000000000000000", sessionUserPhotos);
     // console.log('photo.user_id', photo?.user_id)
-    console.log('sessionUser.id', typeof sessionUser?.id)
+    console.log("sessionUser.id", typeof sessionUser?.id);
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(getPhotos())
-    }, [dispatch])
+        dispatch(getPhotos());
+    }, [dispatch]);
 
-    if (!sessionUser) return <Redirect to='/login' />
+    if (!sessionUser) return <Redirect to="/login" />;
     return (
         <div>
             {sessionUserPhotos?.map(photo => (
-                <img src={photo.image_url} />
+                <NavLink to={`/photos/${photo.id}`}>
+                    <h1>{photo.title}</h1>
+                    <img src={photo.image_url} />
+                </NavLink>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default Photostream
+export default Photostream;
