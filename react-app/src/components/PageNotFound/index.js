8234584042f0d082dead7c516cccd6e66839af3b
Srdan <srdan.vorkapic@outlook.com>
2022-03-19 02:24:24 -0700

    Error page

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -1,9 +1,22 @@
+import { NavLink } from "react-router-dom";
+import errorImage from "../../assets/bb.jpg";
+import "./PageNotFound.css";
+
 const PageNotFound = ({ sessionUser }) => {
+    let link;
+    if (sessionUser) link = "/photostream";
+    else link = "/login";
     return (
         <>
-            <h1>404 srki</h1>
-            <p>Oops! Looks like you followed a bad link.</p>
-            <p>Here's a link to the home page.</p>
+            <h1>Page Not Found</h1>
+            {/* <img src={errorImage} id='error'/> */}
+            <div>
+                <i class="fas fa-exclamation-triangle"></i>
+                <p>Oops! Looks like you followed a bad link.</p>
+            </div>
+            <p>
+                Here's a link to the <NavLink to={link}>home page</NavLink>.
+            </p>
         </>
     );
 };
