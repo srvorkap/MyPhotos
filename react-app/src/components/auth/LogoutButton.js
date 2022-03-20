908024a47ba107e718a38e91bf81f285e75d7a6c
Srdan <srdan.vorkapic@outlook.com>
2022-03-19 20:16:27 -0700

    Navbar done

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logout } from '../../store/session';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../store/session";
+import logoutImage from "../../assets/logout-button.png";
+import "./LogoutButton.css";
 
-const LogoutButton = () => {
-  const dispatch = useDispatch()
-  const onLogout = async (e) => {
-    await dispatch(logout());
-  };
+const LogoutButton = ({ sessionUser }) => {
+    const [showMenu, setShowMenu] = React.useState(false);
+    const dispatch = useDispatch();
 
-  return <button onClick={onLogout}>Logout</button>;
+    const openMenu = () => {
+        if (!showMenu) setShowMenu(true);
+        else setShowMenu(false);
+    };
+
+    const onLogout = async e => {
+        await dispatch(logout());
+    };
+
+    return (
+        <div id="logout">
+            <img src={logoutImage} onClick={openMenu} id="logout-image"></img>
+            <div>
+                {showMenu && (
+                    <>
+                        <p>Hi {sessionUser.username}!</p>
+                        <p onClick={onLogout}>Logout</p>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default LogoutButton;
