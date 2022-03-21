50f9628e984fb5d938369f94eaff1b4d8c49d82b
Srdan <srdan.vorkapic@outlook.com>
2022-03-20 23:46:28 -0700

    Change login page

--- react-app/src/components/CreateAlbumForm/index.js
+++ react-app/src/components/CreateAlbumForm/index.js
@@ -17,25 +17,25 @@ const CreateAlbumForm = ({ sessionUser }) => {
         e.preventDefault();
         const album = {
             title,
-            description
-        }
-        const data = await dispatch(postAlbum(album))
+            description,
+        };
+        const data = await dispatch(postAlbum(album));
 
-        if (data) setErrors(data)
+        if (data) setErrors(data);
 
-        if (!data) history.push('/albums')
+        if (!data) history.push("/albums");
     };
 
     const onCancel = e => {
         e.preventDefault();
-        history.push('/albums');
+        history.push("/albums");
     };
 
     if (!sessionUser) return <Redirect to="/login" />;
 
     return (
-        <div>
-            <div>
+        <div className="signup-login-page">
+            <div className="signup-login-form">
                 <h1>Add New Album</h1>
                 <form onSubmit={onSubmit}>
                     <ul className="errors">
@@ -49,37 +49,39 @@ const CreateAlbumForm = ({ sessionUser }) => {
                             name="title"
                             value={title}
                             onChange={e => setTitle(e.target.value)}
-                            placeholder='Title'
+                            placeholder="Title"
                         />
                     </div>
                     <div>
                         <textarea
-                        type='text'
-                        name="description"
-                        value={description}
-                        onChange={e => setDescription(e.target.value)}
-                        placeholder='Write a description'
-                        rows='20'
-                        cols='80'
+                            type="text"
+                            name="description"
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}
+                            placeholder="Write a description"
+                            rows="20"
+                            cols="80"
                         />
                     </div>
                     <div className="business-buttons-container">
-                        <button
+                        <div
+                        className="signup-login-button"
                             onClick={onSubmit}
                             type="submit"
                             // className="red buttons"
                             // id="create-business-button"
                         >
                             Create
-                        </button>
-                        <button
+                        </div>
+                        <div
+                            className="signup-login-button"
                             type="button"
                             // className="red buttons"
                             // id="create-business-button"
                             onClick={onCancel}
                         >
                             Cancel
-                        </button>
+                        </div>
                     </div>
                 </form>
             </div>
