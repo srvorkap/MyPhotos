e733ef35bd0a9abc0dfbb41808a9eb25e987a0ff
Srdan <srdan.vorkapic@outlook.com>
2022-04-05 01:29:31 -0700

    test

--- app/models/album_photos.py
+++ app/models/album_photos.py
@@ -5,3 +5,4 @@ album_photos = db.Table(
     db.Column('album_id', db.Integer, db.ForeignKey('albums.id'), primary_key=True),
     db.Column('photo_id', db.Integer, db.ForeignKey('photo.id'), primary_key=True)
 )
+safdsadfasdfasdfasdfsadfsadf
